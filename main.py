2916504225ac94ac527fb1095915f8c236c272e1
Ian Paterson <ianhpaterson@gmail.com>
2022-10-01 17:14:02 -0400

    Add schema creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import zipfile
+import subprocess
 from tqdm import tqdm
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -12,20 +13,20 @@ class TqdmUpTo(tqdm):
             self.total = tsize
         return self.update(b * bsize - self.n)  # also sets self.n = b * bsize
 
-default_version = 'v2022.2'
+version = 'v2022.2'
 
-url = "https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/%s.zip" % (default_version)
-filename = "%s/download/%s.zip" % (PROJECT_ROOT, default_version)
+url = 'https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/%s.zip' % (version)
+filename = '%s/download/%s.zip' % (PROJECT_ROOT, version)
 
-print("Downloading baseball database...\n")
+print('Downloading baseball data...\n')
 
 # Download baseball data w/ progress bar
 with TqdmUpTo(unit = 'B', unit_scale=True, unit_divisor=1024, miniters=1) as t:
     urllib.request.urlretrieve(url, filename, t.update_to)
 
-output_directory = "%s/data" % (PROJECT_ROOT)
+output_directory = '%s/data' % (PROJECT_ROOT)
 
-print("\nExtracting baseball database...\n")
+print('\nExtracting baseball data...\n')
 
 # Extract zip containing baseball data
 with zipfile.ZipFile(filename) as archive:
@@ -37,3 +38,13 @@ with zipfile.ZipFile(filename) as archive:
         for info in archive.infolist():
             archive.extract(info.filename, output_directory)
             pbar.update(info.file_size)
+
+print('\nCreating "baseball" database\n')
+
+db_name = 'baseball_db'
+host = 'localhost'
+port = '5432'
+schema_filename = '%s/db/schema.sql' % (PROJECT_ROOT)
+
+subprocess.run(['createdb', db_name])
+subprocess.run(['psql', '-h', host, '-p', port, '-d', db_name, '-f', schema_filename]);
