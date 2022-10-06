717449493423a13244d43646bfedbff319f88b82
Ian Paterson <ianhpaterson@gmail.com>
2022-10-05 21:00:56 -0400

    Remove progress bar and add core data copy query

--- main.py
+++ main.py
@@ -2,49 +2,61 @@ import os
 import urllib.request
 import zipfile
 import subprocess
-from tqdm import tqdm
+import re
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-# urllib progress bar callback class
-class TqdmUpTo(tqdm):
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        return self.update(b * bsize - self.n)  # also sets self.n = b * bsize
 
 version = 'v2022.2'
 
 url = 'https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/%s.zip' % (version)
-filename = '%s/download/%s.zip' % (PROJECT_ROOT, version)
+download_directory = '%s/download' % (PROJECT_ROOT)
+subprocess.run(['mkdir', download_directory])
+filename = '%s/%s.zip' % (download_directory, version)
 
 print('Downloading baseball data...\n')
 
-# Download baseball data w/ progress bar
-with TqdmUpTo(unit = 'B', unit_scale=True, unit_divisor=1024, miniters=1) as t:
-    urllib.request.urlretrieve(url, filename, t.update_to)
+# Download baseball data
+urllib.request.urlretrieve(url, filename)
 
 output_directory = '%s/data' % (PROJECT_ROOT)
+subprocess.run(['mkdir', output_directory])
 
 print('\nExtracting baseball data...\n')
 
 # Extract zip containing baseball data
 with zipfile.ZipFile(filename) as archive:
-    total_size = 0
-    for info in archive.infolist():
-        total_size += info.file_size
-
-    with tqdm(total=total_size, unit = 'B', unit_scale=True, unit_divisor=1024) as pbar:
-        for info in archive.infolist():
-            archive.extract(info.filename, output_directory)
-            pbar.update(info.file_size)
-
-print('\nCreating "baseball" database\n')
+    archive.extractall(output_directory)
 
 db_name = 'baseball'
 host = 'localhost'
 port = '5432'
 schema_filename = '%s/db/schema.sql' % (PROJECT_ROOT)
 
+print('\n\nCreating database named "baseball"...\n')
+
+# Create database
 subprocess.run(['createdb', db_name])
-subprocess.run(['psql', '-h', host, '-p', port, '-d', db_name, '-f', schema_filename])
+
+print('\nCreating schema for "baseball"...\n')
+
+# Create schema
+subprocess.run(['psql', '-h', host, '-p', port, '-d', db_name, '-f', schema_filename], stdout=subprocess.DEVNULL)
+
+core_data_dir = '%s/data/baseballdatabank-2022.2/core' % (PROJECT_ROOT)
+contrib_data_dir = '%s/data/baseballdatabank-2022.2/contrib' % (PROJECT_ROOT)
+copy_query = 'COPY %s FROM \'%s/%s\' WITH DELIMITER \',\' CSV HEADER;\n'
+eol_pattern = re.compile(r'$')
+underscore_pattern = re.compile(r'\_')
+copy_commands = ''
+core_tables = [ 'People', 'Teams_Franchises', 'Teams', 'Parks', 'Managers', 'Fielding', 'Pitching_Post', 'Appearances', 'Batting',
+          'Managers_Half', 'Fielding_OF', 'Pitching', 'Home_Games', 'Batting_Post', 'Teams_Half', 'Series_Post', 'Fielding_Post',
+          'Allstar_Full', 'Fielding_OF_split' ]
+
+for table in core_tables:
+    filename = eol_pattern.sub(r'.csv', underscore_pattern.sub(r'', table))
+    copy_commands += copy_query % (table, core_data_dir, filename)
+
+print('\nCopying core data to "baseball"\n')
+
+# Copy core data
+subprocess.run(['psql', '-h', host, '-p', port, '-d', db_name, '-c', copy_commands])
