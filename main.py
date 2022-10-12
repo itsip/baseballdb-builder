b989ef6d27115b3c1ef9ee534bf79ffdeed5b909
Ian Paterson <ianhpaterson@gmail.com>
2022-10-11 21:42:53 -0400

    Remove whitespace

--- main.py
+++ main.py
@@ -158,11 +158,9 @@ update_query = '''UPDATE home_games SET park_id = parks.id FROM parks WHERE park
 
 subprocess.run(['psql', '-h', HOST, '-p', PORT, '-d', DB_NAME, '-c', update_query], stdout=subprocess.DEVNULL)
 
-
 print('Adding foreign keys...')
 constraints_filename = '%s/db/constraints.sql' % (PROJECT_ROOT)
 subprocess.run(['psql', '-h', HOST, '-p', PORT, '-d', DB_NAME, '-f', constraints_filename], stdout=subprocess.DEVNULL)
-
 
 # Remove temp directories
 subprocess.run(['rm', '-r', data_dir])
